19b0f47771926a41c125fe2613a7f5eea3d946f8
Felix Mezo <felix.mezog@alumnos.upm.es>
2016-05-28 20:00:00 +0200

    Tema 24: Aceptar comentarios

--- models/comment.js
+++ models/comment.js
@@ -1,9 +1,13 @@
-// Definición del modelo de Comments:
 
-module.exports = function(sequelize, DataTypes){
-	return sequelize.define('Comment',
-							{ text: { type: DataTypes.STRING,
-							  validate: { notEmpty: { msg: "Falta comentario" }}
-							}
-						});
+// Definicion del modelo Comments:
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Comment',
+                          { text: { type: DataTypes.STRING,
+                          	        validate: { notEmpty: {msg: "Falta Comentario"}}
+                          	      },
+                            accepted: { type: DataTypes.BOOLEAN,
+      	                                defaultValue: false
+                                      }
+                          });
 };
